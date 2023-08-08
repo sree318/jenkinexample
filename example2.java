773a4d6dcf1b0efcea0675b1319d513cff4fcd78
sabhari sree <>
2023-08-08 11:30:03 +0530

    jenkin commit

---
+++ example2.java
@@ -0,0 +1,5 @@
+public class example2 {
+    public static void main(String[] args) {
+        System.out.println("secoond example");
+    }
+}
---
+++ hellojenkins.java
@@ -0,0 +1,5 @@
+public class hellojenkins{
+public static void main(String[] args) {
+    System.out.println("hello");
+}
+}
