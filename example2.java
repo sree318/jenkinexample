4970f1e056b437aeec2be1bfd07b85125240b9ae
sabhari sree <>
2023-08-08 12:06:51 +0530

    sixth

--- example2.java
+++ example2.java
@@ -1,5 +1,5 @@
 public class example2 {
     public static void main(String[] args) {
-        System.out.println("secownd qwertyee example");
+        System.out.println("secownd qwertyeec example");
     }
 }
