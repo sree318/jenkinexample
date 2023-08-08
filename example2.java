cd6f473dce0dc86f8b69c41e322ae2ff804534f0
sabhari sree <>
2023-08-08 13:04:59 +0530

    git9

--- example2.java
+++ example2.java
@@ -1,5 +1,5 @@
 public class example2 {
     public static void main(String[] args) {
-        System.out.println("secownd success git example");
+        System.out.println("secownd jj example");
     }
 }
