7e14b8d251eb9008a4dc1e54a27ae06be85279c9
sree318 <110028615+sree318@users.noreply.github.com>
2023-08-08 15:59:51 +0530

    Update example2.java

--- example2.java
+++ example2.java
@@ -1,5 +1,5 @@
 public class example2 {
     public static void main(String[] args) {
-        System.out.println("secownd jj exampless");
+        System.out.println("secowndfd jj exampless");
     }
 }
