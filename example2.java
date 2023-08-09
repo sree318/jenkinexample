2a174a53309fb5c466ea2162ecffab99139a69d8
sree318 <110028615+sree318@users.noreply.github.com>
2023-08-09 10:02:36 +0530

    Update 6 example2.java

--- example2.java
+++ example2.java
@@ -1,5 +1,5 @@
 public class example2 {
     public static void main(String[] args) {
-        System.out.println("qqsecowndfd jssj ss 31 eeexampless1");
+        System.out.println("qqsecsowndfd jssj ss 31 eeexampless1");
     }
 }
